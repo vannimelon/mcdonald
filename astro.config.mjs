5e59e79d2a76aef4953ba0a4ed10b90bee6ed34f
vannimelon <cipino3005@gmail.com>
2023-09-26 02:28:16 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vannimelon.github.io",
-  base: "/mcdonald",
-  build: {assetsPrefix:"/mcdonald/"},
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     tailwind(),
     image({
