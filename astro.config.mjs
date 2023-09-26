fad95d03e958df686941651aeb45b7ee04b41b3b
vannimelon <cipino3005@gmail.com>
2023-09-26 03:00:23 +0200

    per favore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vercel from '@astrojs/vercel/serverless';
@@ -11,9 +10,6 @@ export default defineConfig({
   adapter: vercel(),
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     mdx(),
     sitemap(),
   ],
